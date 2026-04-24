342f0ba2e3aa3f32810242d8c47f0cbc4b5d14fe
Kimmoongi0320 <moongi0320@naver.com>
2026-04-25 00:56:15 +0900

    Initial Commit

---
+++ backend/src/main/java/com/github/logi/LogiApplication.java
@@ -0,0 +1,14 @@
+package com.github.logi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class LogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiApplication.class, args);
+	}
+
+}
