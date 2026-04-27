ae240150bda4bca9c5c7b3cadab62a0919deb4f9
Kimmoongi0320 <moongi0320@naver.com>
2026-04-27 23:40:23 +0900

    [#7] feat: Swagger Configuration 생성

---
+++ backend/src/main/java/com/github/logi/global/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.github.logi.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .info(info());
+    }
+
+    private Info info(){
+        return new Info()
+                .title("Logi")
+                .description("2026 캡스톤 디자인  51팀")
+                .version("1.0.0");
+    }
+}
